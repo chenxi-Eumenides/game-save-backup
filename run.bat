ea8d5c792555ada53ecb4729d3f642a7206e52e5
chenxi-Eumenides <1742041477@qq.com>
2025-08-06 02:19:55 +0800

    修复懒狗一键脚本

--- run.bat
+++ run.bat
@@ -1,4 +1,17 @@
+@echo off
+
 uv run main.py
-git add .
-git commit "update"
-pause
+
+for /f %%a in ('git status --porcelain ^| find /c "saves"') do set modified=%%a
+
+if "%modified%"=="0" (
+    echo 本地存档没有修改。
+    pause
+) else (
+    :: 本地仓库有更新
+    echo 更新了 %modified% 个存档文件
+    git add saves
+    git commit -m "更新游戏存档"
+    git push
+    pause
+)
