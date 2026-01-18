682fcb761657ab99069bb2d57a74c66313680306
chenxi-Eumenides <1742041477@qq.com>
2026-01-19 06:15:22 +0800

    懒人脚本显示分隔符，方便查看内容

--- run.bat
+++ run.bat
@@ -1,17 +1,28 @@
 @echo off
 
+echo 开始备份存档：
+echo ↓
 uv run main.py
 
+echo ↑
+echo 开始检查存档更新：
+echo ↓
 for /f %%a in ('git status --porcelain ^| findstr "saves" ^| find /c /v ""') do set modified=%%a
 
 if "%modified%"=="0" (
+    echo ↑
     echo 本地存档没有修改。
     pause
 ) else (
-    :: 本地仓库有更新
-    echo 更新了 %modified% 个存档文件
+    echo ↑
+    echo 更新了 %modified% 个存档文件。
+    echo ↓
     git add saves
     git commit -m "更新游戏存档 by 懒人脚本"
+    echo ↑
+    echo 推送至github仓库
+    echo ↓
     git push
+    echo ↑
     pause
 )
